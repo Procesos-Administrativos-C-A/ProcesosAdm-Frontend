f11fabf4ef518545a1c2b38f039c447c64b96817
Jhonatan <jhonatangarcia6699@gmail.com>
2024-04-04 22:01:53 +0200

    se implemento form controls para el formulario de preoperativos

--- ProcesosAdministrativos/src/app/pages/operador_y_mantenimiento/preoperativos/preoperativos.component.ts
+++ ProcesosAdministrativos/src/app/pages/operador_y_mantenimiento/preoperativos/preoperativos.component.ts
@@ -2,41 +2,62 @@ import { Component, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Preoperativo} from '../../../core/models/preoperativo.model';
 import {EmpleadosPreoperativo} from '../../../core/models/empleados_preoperativo.model';
-import { FormsModule } from '@angular/forms';
-import {ReactiveFormsModule} from '@angular/forms';
+import { FormArray, FormBuilder, FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+
 
 @Component({
   selector: 'app-preoperativos',
   standalone: true,
-  imports: [CommonModule, FormsModule,FontAwesomeModule],
+  imports: [CommonModule, FormsModule,FontAwesomeModule,ReactiveFormsModule],
   templateUrl: './preoperativos.component.html',
   styleUrl: './preoperativos.component.css'
 })
 export class PreoperativosComponent implements OnInit {
 
-  dropdown_preoperativos = signal(false);
-  dropdown_tramites = signal(false);
-  horas_extra = signal(true);
-  
+  preoperativoForm!: FormGroup;
   fecha_hoy = '';
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+
+    this.preoperativoForm = new FormGroup({
+      lugar: new FormControl([[Validators.required, Validators.minLength(6), Validators.maxLength(13)]]) ,
+      fecha: new FormControl(new Date().toISOString().substring(0, 10), [Validators.required]),
+      turno: new FormControl(["", [Validators.required]]),
+      festivo: new FormControl(false),
+      extra: new FormControl(false),
+      estaciones: new FormArray([
+        
+      ])
+    });
+    
+    this.fecha_hoy = new Date().toISOString().substring(0, 10);
+  }
   
 
-  lugares = signal<EmpleadosPreoperativo[]>([
-    {
-      lugar: 'Cambulos',
-      empleados: ['','','']
-    },
-    {
-      lugar: 'Betania',
-      empleados: ['','','']
-    },
-    {
-      lugar: 'Fundadores',
-      empleados: ['','','','']
-    }
-  ]);
+  get estacionesesArray() {
+    return this.preoperativoForm.get('estaciones') as FormArray;
+  }
+  empleadosArray(estacion : number): FormArray {
+    return this.estacionesesArray.at(estacion).get('empleados') as FormArray ;
+    
+  }
+
+  dropdown_preoperativos = signal(false);
+  dropdown_tramites = signal(false);
+  
+  
+
+
+  
+  eliminarEmpleado(index: number) {
+    //(this.preoperativoForm.get('empleados') as FormArray).removeAt(index);
+  }
+
 
   dropDownPre(): void {
     this.dropdown_preoperativos.set(!this.dropdown_preoperativos()) ;
@@ -47,100 +68,90 @@ export class PreoperativosComponent implements OnInit {
   }
 
 
-  ngOnInit(): void {
-    this.fecha_hoy = new Date().toISOString().substring(0, 10);
-  }
-  agregarPersona(index : number){
-    this.lugares.update((lugares) => {
-      return lugares.map((lugar, position) => {
-        if (position == index){
-          return{
-            ...lugar,
-            empleados : [...lugar.empleados, '']
-          }
-        }
-        return lugar;
-      })
-    })
-  }
-  quitarPersona(index: number, lugarIndex: number){
-    this.lugares.update((lugares) => {
-      return lugares.map((lugar, position) => {
-        if (position == lugarIndex){
-          return{
-            ...lugar,
-            empleados: lugar.empleados.filter((empleado, i) => i !== index), 
-          }
-        }
-        return lugar;
-      })
-    })
-  }
 
-  actualizarPersona(index: number, lugarIndex: number, event: Event){
+
+  actualizarLinea(event: Event){
     const elementInput = event.target as HTMLInputElement;
+    let lineas: any[] = [];
 
-    this.lugares.update((lugares) => {
-      return lugares.map((lugar, position) => {
-        if (position == lugarIndex){
-          return{
-            ...lugar,
-            empleados: lugar.empleados.map((empleado, i) => {
-              if (i === index) {
-                return  elementInput.value; // Actualizar nombre
-              }
-              return empleado;
-            }),
-          }
-        }
-        return lugar;
-      })
-    })
-  }
-
-  cambioLinea(event: Event){
-    const elementInput = event.target as HTMLInputElement;
-    
     switch (elementInput.value) {
       case 'Linea1':
-        this.lugares.set([
-          {
-            lugar: 'Cambulos',
-            empleados: ['','','']
-          },
-          {
-            lugar: 'Betania',
-            empleados: ['','','']
-          },
-          {
-            lugar: 'Fundadores',
-            empleados: ['','','','']
-          }
-        ]);
-        return ;
+        lineas = ['Cambulos','Betania','Fundadores'];
+        break;
       case 'Linea2':
-        this.lugares.set([
-          {
-            lugar: 'Villamaria + S.A.V',
-            empleados: ['','','']
-          },
-          {
-            lugar: 'Villamaria + S.U.V',
-            empleados: ['','','']
-          }
-        ]);
-        return ;
+        lineas = ['Villamaria + S.A.V','Villamaria + S.U.V'];
+        break;
       case 'Mantenimiento':
-        this.lugares.set([
-          {
-            lugar: 'Mantenimiento',
-            empleados: ['','','']
-          }
-        ]);
-        return ;
+        lineas = ['Mantenimiento'];
+        break;
       default:
-        this.lugares.set([]);
-        return ;
+        
+        break;
     }
+
+    const estacion = this.preoperativoForm.get('estaciones') as FormArray;
+    estacion.clear();
+    lineas.forEach(element => {
+      
+      const insertable = new FormGroup({
+        nombre: new FormControl([element, [Validators.required]]),
+        empleados: new FormArray([
+          new FormGroup({
+            hora_extra: new FormControl(['0', Validators.pattern(/^[0-9]$/),Validators.maxLength(1)]),
+            puesto: new FormControl(['', Validators.required, Validators.nullValidator]),
+            nombre: new FormControl (['', Validators.required, Validators.nullValidator]),
+          }),
+          new FormGroup({
+            hora_extra: new FormControl(['0', Validators.pattern(/^[0-9]$/),Validators.maxLength(1)]),
+            puesto: new FormControl(['', Validators.required, Validators.nullValidator]),
+            nombre: new FormControl (['', Validators.required, Validators.nullValidator]),
+          }),
+          new FormGroup({
+            hora_extra: new FormControl(['0', Validators.pattern(/^[0-9]$/),Validators.maxLength(1)]),
+            puesto: new FormControl(['', Validators.required, Validators.nullValidator]),
+            nombre: new FormControl (['', Validators.required, Validators.nullValidator]),
+          })
+        ]) 
+      })
+
+      estacion.push(insertable)
+
+    });
+      
   }
+
+
+  
+  agregarEmpleado() {
+    const empleadoFormGroup = new FormGroup({
+      hora_extra: new FormControl (['0', Validators.pattern(/^[0-9]$/),Validators.maxLength(1)]),
+      puesto: new FormControl (['', Validators.required, Validators.nullValidator]),
+      nombre: new FormControl (['', Validators.required, Validators.nullValidator]),
+      cedula: new FormControl (['']),
+    });
+    const empleado = this.preoperativoForm.get('estaciones') as FormArray;
+    for (const empleados of empleado.controls) {
+      console.log(empleados.get('nombre')?.value[0]);
+      if (empleados.get('nombre')?.value[0] === "Cambulos") {
+        const evn = empleados.get('empleados') as FormArray;
+        evn.push(empleadoFormGroup);
+        console.log('ingerido');
+      }
+    }
+    
+    console.log(empleado)
+    //console.log(this.preoperativoForm.get('estaciones') as FormArray)
+    /*
+    const estacionesArray = this.preoperativoForm.get('estaciones') as FormArray;
+    const mantenimientoArray = estacionesArray.at(estacionesIndex).get('mantenimiento') as FormArray;
+    mantenimientoArray.push(empleadoFormGroup);
+
+    (this.preoperativoForm.get('empleados') as FormArray).push(empleadoFormGroup); */
+  }
+
+
+  mostrarInfon(){
+    console.log(this.preoperativoForm.value)
+  }
+
 }
